874add12c3a2c3253dc66c007e8c13d03b659b8d
Jasper <jasper@jazper.dk>
2025-03-16 22:10:49 +0100

    Clear branch

--- src/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JazperDK.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
